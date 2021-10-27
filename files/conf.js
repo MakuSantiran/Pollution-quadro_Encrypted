a3c2870a083455c71707eba55a55b49b9a673e7b
MakuSantiran <73550264+MakuSantiran@users.noreply.github.com>
2021-10-27 17:46:11 +0800

    Add files via upload

---
+++ files/conf.js
@@ -0,0 +1,22 @@
+var items = new Array;
+var question;
+var answer;
+var group;
+var picture;
+
+var DontIncludeFromRemix1 = [""];
+var DontIncludeFromRemix2 = [""];
+var DontIncludeFromRemix3 = [""];
+
+var identificationMode = false;
+var drainBy = -0.005;
+var wrongDamage = 25;
+var examMode = true;
+var PanicMode = true;
+var noMusic = false;
+
+var bgmusic0_PF = 'seriousSoundtrack/bgmusic0.mp3';
+var bgmusic1_PF = 'seriousSoundtrack/bgmusic1.mp3';
+var bgmusic2_PF = 'seriousSoundtrack/bgmusic2.mp3';
+var bgmusic3_PF = 'seriousSoundtrack/bgmusic3.mp3'; 
+var bgmusic4_PF = 'seriousSoundtrack/bgmusic4.mp3'; 
---
+++ questions.js
@@ -0,0 +1,415 @@
+//var drainBy = -0.0005;
+//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS//ITEMS
+
+question = ["is the introduction of harmful materials into the environment"];
+answer = ["Pollution"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["can be natural or created by human activity"];
+answer = ["Pollutants"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Refers to the release of contaminants into the athmosphere"];
+answer = ["Air Pollution"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Contributes to the production of nitrogen oxides, which combine with other chemicals in the atmosphere to form ozone"];
+answer = ["Lightning"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+
+// Source of Air pollutions
+
+question = ["air pollution sources that are plants, soils, and other typically widespread contributors of air emissions associated with living organisms."];
+answer = ["Biogenic"];
+group = "Sources Of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["air emissions relate directly or indirectly to geological activity"];
+answer = ["Geogenic"];
+group = "Sources Of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+//  biogenic sources of air pollution
+
+question = ["The chemical process of nitrification and denitrification in soils generate nitrogen oxides "];
+answer = ["Soils","Soil"];
+group = "Biogenic sources of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Plants emit VOCs including some air toxins such as formaldehyde and methanol."];
+answer = ["Vegetation"];
+group = "Biogenic sources of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Pollens, spores, bacteria, and viruses are another type of natural air emissions"];
+answer = ["Biological Particles"];
+group = "Biogenic sources of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+// GeoGenic sources of air pollution
+
+question = ["Geogenic sources when Volcanoes produce particles and gases"];
+answer = ["Volcanoes"];
+group = "Geogenic sources of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Geogenic sources when Wind blown dust comes from eroded rocks and soil Saharan and other dust storms can distribute particles over long distances"];
+answer = ["Dust"];
+group = "Geogenic sources of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Geogenic sources when this or marine aerosol from breaking waves."];
+answer = ["Sea Salt"];
+group = "Geogenic sources of Air Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+// Scope of Air Pollution From a Source
+
+question = ["Scope of air pollution that contributes to global air pollution, such as a large volcanic eruption"];
+answer = ["Global Distribution"];
+group = "Scope of Air Pollution From a Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Scope of air pollution that is distributed across several states, such as emissions from a power plant"];
+answer = ["Regional Distribution"];
+group = "Scope of Air Pollution From a Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Scope of air pollution that emissions from a dry cleaning establishment"];
+answer = ["Local Distribution"];
+group = "Scope of Air Pollution From a Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+// Stationary Source
+
+question = ["are generally larger stationary sources with a specific, fixed location","It can consist of a collection of emissions-generating operations at a facility or a specific emissions-generating operation at a facility. "];
+answer = ["Point Sources"];
+group = "Stationary Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["are smaller stationary sources that may contribute a significant amount of air pollution collectively"];
+answer = ["Nonpoint sources"];
+group = "Stationary Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+
+// Anthropogenic Source
+
+question = ["Are air pollution sources that are located in specific places"];
+answer = ["Stationary Sources"];
+group = "Anthropogenic Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["an Anthropogenic Source that can move to different locations "];
+answer = ["Mobile Sources"];
+group = "Anthropogenic Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["an Anthropogenic Source that is more towards to fire"];
+answer = ["Certain Burning Practices"];
+group = "Anthropogenic Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["an Anthropogenic Source that includes emissions from building materials and furnishings, heating devices, and substances used inside structures such as paints and cleaning fluids"];
+answer = ["Indoor Sources"];
+group = "Anthropogenic Source";
+picture = "";
+items.push({question,answer,group,picture});
+
+//Criteria Pollutant
+
+question = ["formed in the atmosphere by chemical reactions between nitrogen oxides (NOx) and volatile organic compounds (VOC) in the presence of sunlight"];
+answer = ["Ozone"];
+group = "Air Criteria Pollutants";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["directly emitted from a source, like smokestacks, residential wood burning, prescribed fires, and construction sites.","is also formed in the atmosphere from reactions between airborne chemicals emitted from sources such as power plants, cars, and industries"];
+answer = ["Particulate Matter"];
+group = "Air Criteria Pollutants";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Criteria Pollutants where Motor vehicles are the main source of nitrogen oxides in the U.S.; other sources include power plants and stationary and mobile sources that burn fossil fuels."];
+answer = ["Nitrogen Oxide"];
+group = "Air Criteria Pollutants";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Criteria Pollutants where point sources supply most of these in the U.S., especially coal-burning power plants. "];
+answer = ["Sulfur Oxide"];
+group = "Air Criteria Pollutants";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Criteria Pollutants where Stationary sources that produce these emissions include lthese smelters, waste incinerators, power plants, and these -acid battery manufacturers. What are these??"];
+answer = ["Lead"];
+group = "Air Criteria Pollutants";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Criteria Pollutants where it is emitted by onroad motor vehicles, nonroad engines and vehicles, industrial processes like metals processing and chemical manufacturing, and residential wood burning."];
+answer = ["Carbon Monoxide"];
+group = "Air Criteria Pollutants";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Criteria Pollutants are airborne pollutants that can cause, or may cause, serious health effects."];
+answer = ["Air Toxics","Air Toxic","Toxic Air","Toxic Air"];
+group = "Air Criteria Pollutants";
+picture = "";
+items.push({question,answer,group,picture});
+
+// GreenHouseGasses
+
+question = ["it is generated when fossil fuels like coal, natural gas and oil are burned, and when other materials are burned","produced by chemical reactions during certain industrial processes that do not involve combustion. "];
+answer = ["Carbon Dioxide"];
+group = "Greenhouse Gases";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["its where is also emitted during the production and transport of coal, natural gas, and oil, and as a product of the decay of organic waste in municipal solid waste landfills."];
+answer = ["Methane"];
+group = "Greenhouse Gases";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["it is emitted during soil management and other agricultural practices. It is also produced during certain industrial activities, the combustion of fossil fuels and solid waste, and during the treatment of wastewater."];
+answer = ["Nitrous oxide"];
+group = "Greenhouse Gases";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["It does not occur naturally but are synthesized by humans as the result of a variety of industrial processes, its used as substitutes for stratospheric ozone-depleting substances"];
+answer = ["Fluorinated Gases"];
+group = "Greenhouse Gases";
+picture = "";
+items.push({question,answer,group,picture});
+
+// Trivials
+
+question = ["The air quality of the philippines is considered to be ?"];
+answer = ["Unsafe"];
+group = "Trivials";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["What is the amount of Philippine's ug/m3?"];
+answer = ["18"];
+group = "Trivials";
+picture = "";
+items.push({question,answer,group,picture});
+
+//Definition
+question = ["Refers to the degradation of land due to the presence of chemicals or other man-made substances in this type of material."];
+answer = ["Soil Pollution"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+//Causes Of Land Pollution
+question = ["This refers to the improper disposal of waste products"];
+answer = ["Litter","Garbage"];
+group = "Causes Of Land Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["This occurs when contamination created as a by-product of raising livestock and growing food crops is released into the environment, and the contamination is vast. "];
+answer = ["Agriculture","Farming"];
+group = "Causes Of Land Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["This produces a ton of land pollution in the form of chemicals such as herbicides and pesticides"];
+answer = ["Farming"];
+group = "Causes Of Land Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["theses uses many chemicals in the manufacturing of their specific product which usually get disposed along with the rest of the regular waste. "];
+answer = ["Factories"];
+group = "Causes Of Land Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Happens when lack of forests leads to excess carbon dioxide in our atmosphere"];
+answer = ["Deforestation"];
+group = "Causes Of Land Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["This type of act can can release toxic chemicals into the air and soil.","This type of act can directly destroy surrounding land and its ecosystems, producing large holes in the ground which can cause large scale erosion"];
+answer = ["Mining"];
+group = "Causes Of Land Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["To accommodate this increased population, construction activities also occur, which result in large waste materials, such as metal, plastic, wood, and bricks. What is this called?"];
+answer = ["Urbanization and Construction","Construction and Urbanization"];
+group = "Causes Of Land Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+//Other Types Of Polution
+
+question = ["A type of pollution where its a constant presence of loud, disruptive noises in an area."];
+answer = ["Noise Pollution"];
+group = "Other Types Of Polution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["A type of pollution where its an anthropogenic and artificial light in the environment, especially during night time, exacerbated by excessive, misdirected or obtrusive use of light which fundamentally alters natural conditions"];
+answer = ["Light Pollution"];
+group = "Other Types Of Polution";
+picture = "";
+items.push({question,answer,group,picture});
+//Definition
+
+question = ["Occurs when harmful pollutants and particulate matter are introduced into a water body."];
+answer = ["Water Pollution"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["This water is unsafe for drinking and swimming."];
+answer = ["Polluted Water"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["has been a major problem in developing countries and many people within these countries have no access to clean water or sanitary conditions"];
+answer = ["Sewage disposal"];
+group = "Definition";
+picture = "";
+items.push({question,answer,group,picture});
+
+//Source Of Water Pollution
+
+question = ["Source Of Water Pollution where it is produced from domestic households, industrial practices as well as agricultural practices results in pollution of many of our lakes and rivers"];
+answer = ["Sewage and WasteWater", "WasteWater and Sewage"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Source Of Water Pollution where Litters get caught around limbs of marine animals and many times causes serious health issues and death of animals is becoming commonplace at sea. "];
+answer = ["Marine Dumping"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Source Of Water Pollution where it uses fresh water to carry away wastewater from the plant into the rivers, lakes and oceans"];
+answer = ["Industrial Waste"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Source Of Water Pollution where Medical, scientific and industrial processes that use this material produce nuclear waste are also sources of water pollution"];
+answer = ["Radioactive Waste"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Source Of Water Pollution where it can never dissolve in water and will form a thick sludge in the water"];
+answer = ["Oil"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Source Of Water Pollution where these were constructed prior to 1980 and are made from steel pipes exposed to the environment. The steel corrodes and causes leakages thus causing contamination."];
+answer = ["Underground Storage"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Source Of Water Pollution where the wastewater from agricultural lands can flow into our surface water by rainwater run-off or improper management."];
+answer = ["Agriculture"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["Source Of Water Pollution where these generally sprawl over large areas."];
+answer = ["Landfill Leakage"];
+group = "Source Of Water Pollution";
+picture = "";
+items.push({question,answer,group,picture});
+
+//Industrial Waste Definitions
+
+question = ["these fibers are carcinogenic and a serious health hazard"];
+answer = ["Asbestos","Asbestos Fibers"];
+group = "Industrial Waste Definitions";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["can cause environmental and health problems for animals and humans"];
+answer = ["Lead"];
+group = "Industrial Waste Definitions";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["illness through mercury poisoning has caused adverse effects to humans and animals"];
+answer = ["Mercury"];
+group = "Industrial Waste Definitions";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["fertilizers have nitrates that are being washed from the soil into water sources and causing eutrophication"];
+answer = ["Nitrates and Phosphates","Phosphates and Nitrates"];
+group = "Industrial Waste Definitions";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = [" it is very harmful to marine life if found in large quantities"];
+answer = ["Sulfur"];
+group = "Industrial Waste Definitions";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["it does not dissolve in water and stops marine plants from receiving enough light for photosynthesis. Marine animals and birds are also extremely harmed by oil."];
+answer = ["Oil"];
+group = "Industrial Waste Definitions";
+picture = "";
+items.push({question,answer,group,picture});
+
+// Trivials
+
+question = ["This country is the Largest contributing country estimated dumping more than 356,371 metric tons of plastic waste annually. "];
+answer = ["Philippines"];
+group = "Trivials";
+picture = "";
+items.push({question,answer,group,picture});
+
+question = ["The world's most polluting river accounting for 63,000 tons of plastic entering oceans from rivers per year is known as?"];
+answer = ["Pasig River"];
+group = "Trivials";
+picture = "";
+items.push({question,answer,group,picture});
+
+/**/
+/*
+*/
